37f747f6cf1179bf6c5c29c782821e3acd5797cf
Mathias Andresen <mathias@3mogs.dk>
2014-10-07 19:20:24 +0200

    Uploaded files

---
+++ Assets/ClickToMove.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	Vector3 clickPosition;
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		clickPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			GetMousePosition();
+		}
+
+	}
+
+	void FixedUpdate(){
+		MoveToPoint ();
+	}
+
+	void GetMousePosition(){
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if(Physics.Raycast(ray, out hit, 1000)){
+			clickPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			Debug.Log(clickPosition);
+		}
+	}
+
+	void MoveToPoint(){
+		if (Vector3.Distance (transform.position, clickPosition) > 2.5) {
+			Quaternion newRotate = Quaternion.LookRotation (clickPosition - transform.position, transform.forward);
+			newRotate.x = 0;
+			newRotate.z = 0;
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotate, 0.1f);
+
+			rigidbody.AddForce(transform.forward*speed);
+		}
+
+	}
+}
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int curHealth = 100;
+	
+	void Start () {
+	}
+
+	void Update () {
+		if (curHealth >= maxHealth) {
+			curHealth = maxHealth;		
+		}
+		if (curHealth <= 0) {
+			curHealth = 0;		
+		}
+	}
+
+	public void ApplyDamage(int damage){
+		curHealth -= damage;
+	}
+
+	public void Heal(int heal){
+		curHealth += heal;
+	}
+}
